076e91540068a340d3bc4fb07537e252d2a6d0e5
xuqigang <xuqigang10@163.com>
2019-06-09 23:49:20 +0800

    更新版本

---
+++ LTNetworking/Classes/ReplaceMe.m
@@ -0,0 +1,7 @@
+/*打开终端一顿操作猛如虎
+ cd /Users/yufu/Desktop/LTMainProject/LTMainProject
+ vim Podfile
+ 添加source ‘https://git.coding.net/lt1061961387/LTSpec.git’
+ 添加pod 'LTNetworking'后退出
+ pod install
+ 打开主工程发现组件已经乖乖躺在Pod里了*/
